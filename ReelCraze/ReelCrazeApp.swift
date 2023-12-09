67ed8a0e25ee7af41aec90f08c4cf169ef31ce4d
PhoonThetPine <thetpine254@gmail.com>
2023-12-09 10:11:29 +0800

    Initial Commit

---
+++ ReelCraze/ReelCrazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelCrazeApp.swift
+//  ReelCraze
+//
+//  Created by Phoon Thet Pine on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ReelCrazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
