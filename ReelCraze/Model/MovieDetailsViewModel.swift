a91f4b45f2005f716ee482a157f805b22cc79446
PhoonThetPine <thetpine254@gmail.com>
2023-12-25 22:07:58 +0800

    ReelCraze - V3

---
+++ ReelCraze/Model/MovieDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailsViewModel.swift
+//  ReelCraze
+//
+//  Created by Phoon Thet Pine on 10/12/23.
+//
+
+import Foundation
--- ReelCraze/Model/MovieDiscoverViewModel.swift
+++ ReelCraze/Model/MovieDiscoverViewModel.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 @MainActor
-class MovieDBViewModel: ObservableObject {
+class MovieDiscoverViewModel: ObservableObject {
     
     @Published var trending: [Movie] = []
     @Published var searchResults: [Movie] = []
+//    @Published var movie
+    
     static let apiKey = "b7be815eb9592c94b6990ee4246aaaf7"
     //static let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2JlODE1ZWI5NTkyYzk0YjY5OTBlZTQyNDZhYWFmNyIsInN1YiI6IjY1NzNkMDlmYmJlMWRkMDBmZTJhYmY3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KmIKbwqwPNTeSa5r9dj6YP59XONO8O9SH327KLo9Nks"
     
@@ -19,7 +21,7 @@ class MovieDBViewModel: ObservableObject {
     
     func loadTrending() {
         Task {
-            let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day?api_key=\(MovieDBViewModel.apiKey)")!
+            let url = URL(string: "https://api.themoviedb.org/3/trending/movie/day?api_key=\(MovieDiscoverViewModel.apiKey)")!
             
             do {
                 let (data, _) = try await URLSession.shared.data(from: url)
@@ -35,7 +37,7 @@ class MovieDBViewModel: ObservableObject {
     func search(term: String) {
         Task {
             
-            let url = URL(string: "https://api.themoviedb.org/3/search/movie?api_key=\(MovieDBViewModel.apiKey)&language=en=US&page=1&include_adult=false&query=\(term)"
+            let url = URL(string: "https://api.themoviedb.org/3/search/movie?api_key=\(MovieDiscoverViewModel.apiKey)&language=en=US&page=1&include_adult=false&query=\(term)"
                 .addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)!)!
             
             do {
@@ -46,5 +48,34 @@ class MovieDBViewModel: ObservableObject {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func movieCredits(for movieID: Int) {
+        Task {
+            let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/credits?api_key=\(MovieDiscoverViewModel.apiKey)&language=en=US")!
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let credits = try JSONDecoder().decode(MovieCredits.self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+class MovieDetailsViewModel: ObservableObject {
+    
+}
+
+struct MovieCredits: Decodable {
+    
+    let id: Int
+    let cast: [Cast]
+    
+    struct Cast: Decodable {
+        let name: String
+        let id: Int
+        let character: String
     }
 }
